fdea219e42f822cdc7f73c24b40b13baaab86fda
MohamedSaffan <201827254@std.sci.cu.edu.eg>
2020-09-01 17:15:57 +0200

    finishes Our App

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,18 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
-
+import flv from "flv.js";
 const StreamShow = (props) => {
   const streamId = props.match.params.id;
+
+  const videoEl = useRef(null);
+  const stream = null || props.stream;
+  console.log(stream, " from stream");
+
   useEffect(() => {
     props.fetchStream(streamId);
   }, []);
+
+  useEffect(() => {
+    if (!stream) {
+      return;
+    }
+
+    const player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${streamId}.js`,
+    });
+    player.attachMediaElement(videoEl.current);
+    player.load();
+
+    return () => {
+      player.destroy();
+      console.log("media destoryed");
+    };
+  }, [stream]);
 
   const renderStream = () => {
     if (!props.stream) return <div>Loading....</div>;
 
     return (
       <div>
+        <video ref={videoEl} controls style={{ width: "100%" }} />
         <h2>{props.stream.title}</h2>
         <p>{props.stream.description}</p>
       </div>
